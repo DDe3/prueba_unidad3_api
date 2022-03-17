d756d310819658e76c84c8387926f04c5d27706f
DDe3 <82774718+DDe3@users.noreply.github.com>
2022-03-16 20:22:17 -0500

    Final commit

--- prueba_backend/src/main/java/com/example/demo/controller/CitaRestController.java
+++ prueba_backend/src/main/java/com/example/demo/controller/CitaRestController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.repository.modelo.CitaMedica;
 import com.example.demo.service.ICitaMedicaService;
 
 @RestController
@@ -13,6 +20,11 @@ public class CitaRestController {
 	
 	@Autowired
 	private ICitaMedicaService citaService;
+	
+	@GetMapping(path = "/{cedula}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<CitaMedica>> getCitasPendientes(@PathVariable("cedula") String cedula) {
+		return ResponseEntity.ok(citaService.consultarCitasPendientes(cedula));
+	}
 	
 	
 
